860129ff8f41818b26c97b69dff5cfeb5eaf994f
Henry Bley-Vroman <olets@olets.dev>
2025-02-05 14:58:08 -1000

    feat(VersionNotice): v4 has been released

--- docs/.vitepress/components/VersionNotice.vue
+++ docs/.vitepress/components/VersionNotice.vue
@@ -1,21 +1,19 @@
 <template>
-  <div class="danger custom-block">
-    <p class="custom-block-title">Is this the page you’re looking for?</p>
-    <p>
-      These docs are for the not-yet-released v4.<br />
-      For v3 docs visit
-      <a
-        href="https://v3.zsh-abbr.olets.dev"
-        target="_self"
-        rel="noreferrer"
-        class="vp-external-link-icon"
-        >v3.zsh-abbr.olets.dev</a
-      >.<br />
-      Learn about upgrading from v3 to v4 in the
-      <a href="./migrating-between-versions.html" data-fathom-initialized=""
-        >migration guide</a
-      >.
-    </p>
-  </div>
-  <p>&nbsp;</p>
+    <div class="tip custom-block">
+        <p class="custom-block-title">
+            These docs are for the newly-released v4.
+        </p>
+        <p>
+            If you are or were using v3, check out
+            <a href="/whats-new.html">What's New</a>. If you are or were using
+            _and_ do advanced configuration, read the
+            <a href="./migrating-between-versions.html">migration guide</a>
+            and/or read about
+            <a href="https://v3.hometown-prompt.olets.dev/installation.html"
+                >pinning v3.x</a
+            >.
+        </p>
+    </div>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
 </template>
