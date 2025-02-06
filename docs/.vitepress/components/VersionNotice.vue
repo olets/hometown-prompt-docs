13270fa25334aa60b6e04bca69976c956f3620c0
Henry Bley-Vroman <olets@olets.dev>
2025-02-06 07:08:35 -1000

    fix(VersionNotice): copy edit

--- docs/.vitepress/components/VersionNotice.vue
+++ docs/.vitepress/components/VersionNotice.vue
@@ -6,7 +6,7 @@
         <p>
             If you are or were using v3, check out
             <a href="/whats-new.html">What's New</a>. If you are or were using
-            <em>and</em> do advanced configuration, read the
+            v3 <em>and</em> do advanced configuration, read the
             <a href="./migrating-between-versions.html">migration guide</a>
             and/or read about
             <a href="https://v3.hometown-prompt.olets.dev/installation.html"
